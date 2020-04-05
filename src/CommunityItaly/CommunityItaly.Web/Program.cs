d39923a06a73084200bf379ba0c1400bd4beb4f7
Andrea Tosato <andrea.tosato@hotmail.it>
2020-04-05 10:58:31 +0200

    Update Blazor

--- src/CommunityItaly/CommunityItaly.Web/Program.cs
+++ src/CommunityItaly/CommunityItaly.Web/Program.cs
@@ -1,19 +1,22 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CommunityItaly.Web
 {
 	public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+			builder.Services.AddBaseAddressHttpClient();
 
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
